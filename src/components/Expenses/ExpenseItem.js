71d276971f27e64e501da12e0c2f4018611c3e1f
iostate <qmtruong92@gmail.com>
2022-08-18 11:25:40 -0700

    Add Event Handler to change title of Expenses

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.expenseTitle);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.expenseDate} />
       <div className='expense-item__description'>
-        <h2>{props.expenseTitle}</h2>
+        {/* <h2>{props.expenseTitle}</h2> */}
+        <h2>{title}</h2>
         <div className='expense-item__price'>{props.expenseAmount}</div>
+        <button onClick={clickHandler}>Change Title!</button>
       </div>
     </Card>
   );
