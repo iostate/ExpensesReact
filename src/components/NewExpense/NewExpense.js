382acbd6d56207f433b71c7868febf5861706981
iostate <qmtruong92@gmail.com>
2022-08-18 12:33:35 -0700

    Pass data from child to parent

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,17 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
+  const saveExpenseHandler = (userExpenseInput) => {
+    const expenseData = {
+      ...userExpenseInput,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
+
   return (
     <div className='new-expense'>
-      <ExpenseForm />
+      <ExpenseForm onSaveExpense={saveExpenseHandler} />
     </div>
   );
 };
