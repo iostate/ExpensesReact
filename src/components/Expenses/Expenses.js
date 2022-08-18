613c81f6f68feb22d153b5c40ba81f19be99a463
iostate <qmtruong92@gmail.com>
2022-08-18 14:57:18 -0700

    I'm too good at this

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpensesFilter from './ExpenseFilter';
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
@@ -8,20 +10,32 @@ function round(value, decimals) {
 }
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = (selectedYear) => {
+    console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <div className='expenses-main'>
-      {props.expenses.map((el, key) => {
-        console.log(el.expenseDate);
-        return (
-          <ExpenseItem
-            key={key}
-            expenseAmount={round(getRandomArbitrary(7.12, 12922.23), 2)}
-            expenseDate={el.expenseDate}
-            expenseTitle={el.expenseTitle}
-          />
-        );
-      })}
+    <div>
+      <div className='expenses-main'>
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {props.expenses.map((el, key) => {
+          return (
+            <ExpenseItem
+              key={key}
+              expenseAmount={round(getRandomArbitrary(7.12, 12922.23), 2)}
+              expenseDate={el.expenseDate}
+              expenseTitle={el.expenseTitle}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
 export default Expenses;
+
+// listen to changes on dropdown
+// when user select new year (new state)
+// listen to that event, and send PickedYear to Expenses component
